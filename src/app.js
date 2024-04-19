43cec31239d5896882e29abd004665d990bf75d6
krausm-bzz <krausm@bzz.ch>
2024-04-19 11:03:48 +0200

    all /login & authentication done

--- src/app.js
+++ src/app.js
@@ -8,8 +8,24 @@ const session = require('express-session')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
+
+function authentication(req, res, next) {
+    if (req.session.isAuthenticated) {
+        next()
+    } else {
+        res.status(403).json({ error: "Unauthorized" });
+    }
+}
 
 let tasks = [{id: 1, titel: "Einkaufsliste", beschreibung: "Milch, Brot, Eier kaufen", done: false, dueDate: "2024-04-20"}]
+
+app.use('/tasks', authentication)
 
 app.get('/tasks', (req,res) => {
     return res.status(200).json(tasks);
@@ -58,6 +74,36 @@ app.delete('/tasks/:id', (req, res) => {
         tasks.splice(task, 1)
         res.status(200).json(task)
     }
+})
+
+const loginData = [{ email: '', password: 'm295'}]
+let isAuthenticated = false
+
+app.post('/login', (req, res) => {
+    const { email, password} = req.body
+    const user = loginData.find(user => user.password === password)
+    if (user) {
+        user.email = email
+        req.session.user = user
+        isAuthenticated = true
+        req.session.isAuthenticated = isAuthenticated
+        res.status(201).json({ authorisation: "true" })
+    } else {
+        res.status(401).send('Unautorisiert')
+    }
+})
+
+app.get('/verify', authentication, (req, res) => {
+    if (isAuthenticated) {
+        res.status(201).json({ email: req.session.user.email })
+    } else {
+        res.status(203).send('Unautorisiert')
+    }
+})
+
+app.delete('/logout', authentication, (req, res) => {
+    isAuthenticated = false
+    res.status(204)
 })
 
 
