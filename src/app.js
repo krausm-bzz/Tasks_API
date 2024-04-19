c20365d504f2e48c6bdc01b7f591926d0724bd86
krausm-bzz <krausm@bzz.ch>
2024-04-19 10:33:28 +0200

    1. /get /tasks/:id & /put /tasks/:id done

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const session = require('express-session')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-let tasks = [{"id": 1, "titel": "Einkaufsliste", "beschreibung": "Milch, Brot, Eier kaufen", "done": false, "dueDate": "2024-04-20"}]
+let tasks = [{id: 1, titel: "Einkaufsliste", beschreibung: "Milch, Brot, Eier kaufen", done: false, dueDate: "2024-04-20"}]
 
 app.get('/tasks', (req,res) => {
     return res.status(200).json(tasks);
@@ -27,6 +27,28 @@ app.post('/tasks', (req, res) => {
         res.status(422).json({error: "titel erfordelich"})
     }
 });
+
+app.get('/tasks/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const task = tasks.find(task => task.id === id)
+    if (!id) {
+        res.status(404).json({ error: 'Task nicht gefunden' })
+    } else {
+        res.status(200).json(task)
+    }
+})
+
+app.put('/tasks/:id', (req, res) => {
+    tasks = tasks.map((task) => {
+        if(task.id === parseInt(req.params.id)) {
+            return {...task, ...req.body};
+        } else {
+            return task;
+        }
+    })
+    res.send(tasks);
+});
+
 
 app.listen(port, () => {
     console.log(`Tasks API listening on port ${port}`);
