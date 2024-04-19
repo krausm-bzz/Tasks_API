f48c8401dc5d6143e4f8c4895569b71ee8be1a8f
krausm-bzz <krausm@bzz.ch>
2024-04-19 09:46:08 +0200

    1. /post updatet

--- src/app.js
+++ src/app.js
@@ -1,38 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
+//const swaggerUi = require('swagger-ui-express')
+//const swaggerDocument = require('./swagger.json')
 const session = require('express-session')
-app.use('/swagger-ui/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+//app.use('/swagger-ui/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-let tasks = []
+let tasks = [{"id": 1, "titel": "Einkaufsliste", "beschreibung": "Milch, Brot, Eier kaufen", "done": false, "dueDate": "2024-04-20"}]
 
 app.get('/tasks', (req,res) => {
     return res.status(200).json(tasks);
 })
 
 app.post('/tasks', (req, res) => {
-    const {Titel, Beschreibung, DueDate} = req.body;
-
-
-    if (!Titel || !Beschreibung || !DueDate) {
-        return res.status(422).json({error: "Titel, Beschreibung und DueDate sind erforderlich."});
+    const { titel, beschreibung, dueDate } = req.body
+    if (titel) {
+        const id = (tasks.length + 1).toString()
+        let completed = false
+        const newTask = { id, titel, beschreibung , dueDate, completed}
+        tasks.push(newTask)
+        res.status(201).json(newTask)
+    } else {
+        res.status(422).json({error: "titel erfordelich"})
     }
-
-    const task = {
-        ID: tasks.length + 1, // auto increment
-        Titel,
-        Beschreibung,
-        Done: false,
-        DueDate,
-        Erstellungsdatum: new Date().toISOString(),
-        Erfuellungsdatum: null
-    };
-})
+});
 
 app.listen(port, () => {
     console.log(`Tasks API listening on port ${port}`);
